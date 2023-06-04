44a76737be28f0aca2ca6eb2bf30f27c5ccc17cd
root <wkdeogks17@gmail.com>
2023-06-04 05:32:47 +0000

    Login

--- frontend/src/login.js
+++ frontend/src/login.js
@@ -14,8 +14,15 @@ import Logo from "./goals.png"
 				<h1 className="text-4xl font-medium text-black">milichiever</h1>
 			  </header>
 			</div>
-			<form className="w-11/12 mt-3 bg-white rounded-lg h-96 mx-auto mb-16">
-			</form>
+			<div className="flex flex-col items-center w-11/12 mt-3 bg-white rounded-lg mx-auto mb-16">
+				<form action="#" className="flex flex-col items-center w-11/12 mt-3 bg-white rounded-lg mx-auto mb-3">
+					<input type="text" placeholder="군번" className="w-10/12 mt-5 px-5 mb-3 mx-auto h-16 rounded-lg bg-neutral-200" />
+					<input type="password" placeholder="비밀번호" className="w-10/12 mx-auto mb-5 px-5 h-16 rounded-lg bg-neutral-200" />
+					<input type="submit" value="로그인" className="w-10/12 mx-auto h-16 rounded-lg bg-sky-600 text-white font-bold" />
+				</form>
+				<a href="https://react-dcsof.run.goorm.site/">비밀번호를 잃어버렸나요?</a>
+				<a href="https://react-dcsof.run.goorm.site/" className="w-11/12 mx-auto h-16 rounded-lg bg-sky-600 text-white font-bold">회원가입</a>
+			</div>
 		</div>
 	)
 }
