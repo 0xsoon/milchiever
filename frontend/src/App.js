1c1bf90c45d99c5a43c56113b9f49405c35dbbd7
0xsoon <kevinh9618@gmail.com>
2023-06-04 14:52:01 +0000

    Restructured frontend app and made login look similar to app layout

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Login from './login';
 import Layout from './components/Layout';
+import Login from './pages/Login';
 import Home from './pages/Home';
 
 import "./styles/tailwind.css";
@@ -10,9 +10,9 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path="/login" element={<Login />}/>
         <Route path="/" element={<Layout />} >
           <Route index element={<Home />} />
-          <Route path="login" element={<Login />} />
         </Route>
       </Routes>
     </BrowserRouter>
