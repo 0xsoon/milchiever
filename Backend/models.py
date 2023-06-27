8677b6a34f68963b17ebd91c4d2b14f781bdf8f1
0xsoon <kevinh9618@gmail.com>
2023-06-27 16:36:22 +0900

    Adds req.txt and modified sql models

--- Backend/models.py
+++ Backend/models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import ForeignKey, Column, Integer, String, Date
+from sqlalchemy import ForeignKey, Column, Integer, String, Date , Numeric
 from sqlalchemy.orm import relationship
 from database import Base
 
@@ -23,12 +23,20 @@ class User(Base):
 class Goal(Base):
     __tablename__ = "goal"
     id = Column(Integer, primary_key=True)
+    title = Column(String, nullable=False)
     startDay = Column(Date, nullable=False)
     endDay = Column(Date, nullable=False)
     content = Column(String, nullable = False)
     
     creatorId = Column(String, ForeignKey('user.username'))
     creator = relationship("User", back_populates="goals")
+
+class SubGoal(Base):
+    __tablename__ = "subgoal"
+    id = Column(Integer, primary_key=True)
+    mainGoalId = Column(Integer, secondary_key=True)
+    title = Column(String, nullable=False)
+    duration= Column(Numeric, nullable=False)
     
 class Schedule(Base):
     __tablename__ = "schedule"
